3d731f0bdec0868bf21010add6337885c8ab0f39
K44aue <128631337+K44aue@users.noreply.github.com>
2023-04-25 16:11:33 -0300

    ATIVIDADE DE VERIFICAÇÃO DE IDADE

---
+++ gabrielm.py
@@ -0,0 +1,10 @@
+idade =int(input("coloque sua idade:"))
+
+if(idade >=18 and idade <50):
+    print("Parabéns você já pode ser preso")
+
+elif(idade >=50):
+    print("Nossa você ainda está vivo!")
+
+else:
+    print("Deu sorte mas sua hora vai chegar")
